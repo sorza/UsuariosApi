8e7d85471b993ed4fedc2245297ac11050c11939
sorza <alexandre.sorza@outlook.com>
2023-10-10 20:36:21 -0300

    Initial commit

---
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
+using UsuariosApi.Services;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private UsuarioService _usuarioService;
+
+        public UsuarioController(UsuarioService cadastroService)
+        {
+           _usuarioService = cadastroService;
+        }
+
+        [HttpPost("cadastro")]
+        public async Task<IActionResult> CadastraUsuario(CreateUsuarioDto dto)
+        {
+            await _usuarioService.Cadastra(dto);
+            return Ok("Usuário Cadastrado!");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(LoginUsuarioDto dto)
+        {
+            var token = await _usuarioService.Login(dto);
+            return Ok(token);
+        }
+    }
+}
